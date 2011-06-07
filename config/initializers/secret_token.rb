abaf2a88241db86f790f3217a4d7f16aa763cee4
Brandon Liu <brandon.k.liu@gmail.com>
2011-06-06 23:46:07 -0400

    Init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RemindavaxPilot::Application.config.secret_token = '2fd5793a3d0045db654ff2e26c5173ca00f0a56745514b836e6c14ce680bc623f11be2c80ab0550ac1a181034ccd8b4210a73038478b772ddf449fe2b837a5b0'
