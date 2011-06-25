c605ec8b90fa91e43880a5da20d155c1e9c51b6c
Brandon Liu <brandon.k.liu@gmail.com>
2011-06-25 10:22:58 -0400

    Changed javascript for check-in
    Before, it depended upon table#patients, which allowed for only one table to have check-ins per page. Now it just depends on td.check-in, which allows for more flexibility

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,10 +3,15 @@
 
 // Initialize the patients/index check in buttons
 $(function() {
-  $("table#patients tbody tr").each(function(i, elt) {
+  $("td.check-in").each(function(i, elt) {
     patient_id = $(this).attr("patient");
     checked_in = $(this).attr("checked_in") === "true";
-    set_checked_in(patient_id, checked_in);
+    if (checked_in) {
+      $(this).html('<span class="checked_in">Checked in</span>');
+    }
+    else {
+      $(this).html('<a href="/patients/' + patient_id + '/check_in" data-method="post" data-remote="true" rel="nofollow">Check in</a>');
+    }
   });
 
   $('input[datepicker="true"]').datepicker({ dateFormat: "M-dd-yy"});
@@ -22,13 +27,3 @@ $(function() {
   var current_time = new Date();
   $.cookie('time_zone', current_time.getTimezoneOffset());
 });
-
-function set_checked_in(patient_id, checked_in) {
-  elt = $("table#patients tr[patient=" + patient_id + "] td:first-child").first();
-  if (checked_in) {
-    elt.html('<span class="checked_in">Checked in</span>');
-  }
-  else {
-    elt.html('<a href="/patients/' + patient_id + '/check_in" data-method="post" data-remote="true" rel="nofollow">Check in</a>');
-  }
-}
