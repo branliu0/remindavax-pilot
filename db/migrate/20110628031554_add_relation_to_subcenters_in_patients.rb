a8231e82d50a00e4cde4f23213abfd3af6daa816
Brandon Liu <brandon.k.liu@gmail.com>
2011-06-28 06:20:44 -0400

    Fixed the column name in the migration

--- db/migrate/20110628031554_add_relation_to_subcenters_in_patients.rb
+++ db/migrate/20110628031554_add_relation_to_subcenters_in_patients.rb
@@ -1,11 +1,11 @@
 class AddRelationToSubcentersInPatients < ActiveRecord::Migration
   def self.up
     add_column :patients, :subcenter_id, :integer
-    remove_column :patients, :subcenter
+    remove_column :patients, :encrypted_subcenter
   end
 
   def self.down
     remove_column :patients, :subcenter_id
-    add_column :patients, :subcenter, :string
+    add_column :patients, :encrypted_subcenter, :string
   end
 end
