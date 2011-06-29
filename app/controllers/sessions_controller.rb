78f34495a2d9677216919b4258e716f91d6c270f
Brandon Liu <brandon.k.liu@gmail.com>
2011-06-29 16:30:44 -0400

    Fixed a typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.authenticate(params[:session][:username], params[:session][:password])
     if user.nil?
-      flash.now[:error] = "Invalid email/password combination"
+      flash.now[:error] = "Invalid username/password combination"
       render :new
     else
       login user
