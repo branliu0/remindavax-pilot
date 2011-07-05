18b6a9371c3352a4481fb84150f163486a1cb09b
Brandon Liu <brandon.k.liu@gmail.com>
2011-07-05 16:57:17 -0400

    Added errors for unique taayi card number

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -48,8 +48,9 @@ class Patient < ActiveRecord::Base
     value :name => 'yes'
     value :name => 'no'
   end
-  validates :taayi_card_number, :uniqueness => true, :numericality => true, :if => Proc.new{ |p| !p.taayi_card_number.blank? }
+  validates :taayi_card_number, :numericality => true, :if => Proc.new{ |p| !p.taayi_card_number.blank? }
   validates_length_of :taayi_card_number, :is => 7, :if => Proc.new{ |p| !p.taayi_card_number.blank? }
+  validates_uniqueness_of :encrypted_taayi_card_number, :if => Proc.new{ |p| !p.taayi_card_number.blank? }
   validates :ec_number, :presence => true, :numericality => true, :length => { :within => 2..3 }
   validates :expected_delivery_date, :presence => true
   validates :caste, :presence => true
