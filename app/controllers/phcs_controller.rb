cd68ade324b3c26ee64bd301f00b3c1327509178
Brandon Liu <brandon.k.liu@gmail.com>
2011-09-04 14:27:31 -0400

    Fix a few bugs for the PHC summary page

--- app/controllers/phcs_controller.rb
+++ app/controllers/phcs_controller.rb
@@ -34,17 +34,17 @@ class PhcsController < ApplicationController
 
     phcs.each do |phc|
       stats = Hash.new
-      Patient.creation_stats_by_week(current_user.phc).each { |week| each_block.call(:patient_count, week) }
-      Appointment.creation_stats_by_week(current_user.phc).each { |week| each_block.call(:appointment_count, week) }
-      Visit.creation_stats_by_week(current_user.phc).each { |week| each_block.call(:visit_count, week) }
-      Sms.creation_stats_by_week(current_user.phc).each { |week| each_block.call(:sms_count, week) }
+      Patient.creation_stats_by_week(phc).each { |week| each_block.call(stats, :patient_count, week) }
+      Appointment.creation_stats_by_week(phc).each { |week| each_block.call(stats, :appointment_count, week) }
+      Visit.creation_stats_by_week(phc).each { |week| each_block.call(stats, :visit_count, week) }
+      Sms.creation_stats_by_week(phc).each { |week| each_block.call(stats, :sms_count, week) }
       stats.map do |yrwk, data|
         stats[yrwk][:week_start] = Date.commercial(yrwk / 100, yrwk % 100, 1)
         stats[yrwk][:week_end] = Date.commercial(yrwk / 100, yrwk % 100, 7)
       end
       stats = stats.to_a.sort{ |a,b| b[0] <=> a[0] } # by yrwk DESC
         .map{ |x| x[1] } # Just make it an array of hashes, discarding the yrwk key
-      @creation_stats << stats
+      @creation_stats << [phc.name, stats]
     end
   end
 end
