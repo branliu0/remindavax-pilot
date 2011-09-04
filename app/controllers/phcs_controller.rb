970a7b2e8f133b5caf6c2df970b8dab37545068b
Brandon Liu <brandon.k.liu@gmail.com>
2011-09-04 14:20:36 -0400

    Update summary page to show all stats for admin

--- app/controllers/phcs_controller.rb
+++ app/controllers/phcs_controller.rb
@@ -25,20 +25,26 @@ class PhcsController < ApplicationController
   end
 
   def summary
-    @creation_stats = Hash.new()
-    each_block = Proc.new do |key, week|
-      @creation_stats[week.yrwk] ||= {}
-      @creation_stats[week.yrwk].merge!(key => week.count)
+    phcs = current_user.admin? ? Phc.all : [ current_user.phc ]
+    @creation_stats = []
+    each_block = Proc.new do |hash, key, week|
+      hash[week.yrwk] ||= {}
+      hash[week.yrwk].merge!(key => week.count)
     end
-    Patient.creation_stats_by_week(current_user.phc).each { |week| each_block.call(:patient_count, week) }
-    Appointment.creation_stats_by_week(current_user.phc).each { |week| each_block.call(:appointment_count, week) }
-    Visit.creation_stats_by_week(current_user.phc).each { |week| each_block.call(:visit_count, week) }
-    Sms.creation_stats_by_week(current_user.phc).each { |week| each_block.call(:sms_count, week) }
-    @creation_stats.map do |yrwk, data|
-      @creation_stats[yrwk][:week_start] = Date.commercial(yrwk / 100, yrwk % 100, 1)
-      @creation_stats[yrwk][:week_end] = Date.commercial(yrwk / 100, yrwk % 100, 7)
+
+    phcs.each do |phc|
+      stats = Hash.new
+      Patient.creation_stats_by_week(current_user.phc).each { |week| each_block.call(:patient_count, week) }
+      Appointment.creation_stats_by_week(current_user.phc).each { |week| each_block.call(:appointment_count, week) }
+      Visit.creation_stats_by_week(current_user.phc).each { |week| each_block.call(:visit_count, week) }
+      Sms.creation_stats_by_week(current_user.phc).each { |week| each_block.call(:sms_count, week) }
+      stats.map do |yrwk, data|
+        stats[yrwk][:week_start] = Date.commercial(yrwk / 100, yrwk % 100, 1)
+        stats[yrwk][:week_end] = Date.commercial(yrwk / 100, yrwk % 100, 7)
+      end
+      stats = stats.to_a.sort{ |a,b| b[0] <=> a[0] } # by yrwk DESC
+        .map{ |x| x[1] } # Just make it an array of hashes, discarding the yrwk key
+      @creation_stats << stats
     end
-    @creation_stats = @creation_stats.to_a.sort{ |a,b| b[0] <=> a[0] } # by yrwk DESC
-      .map{ |x| x[1] } # Just make it an array of hashes, discarding the yrwk key
   end
 end
