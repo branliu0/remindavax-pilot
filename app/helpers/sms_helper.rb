d40c16692fff4623af7ef9d975d318334da6d90f
Brandon Liu <brandon.k.liu@gmail.com>
2012-06-12 13:48:11 -0400

    Update SMS credentials

--- app/helpers/sms_helper.rb
+++ app/helpers/sms_helper.rb
@@ -6,7 +6,8 @@ module SmsHelper
   end
 
   # TODO: Abstract this to a config file? Right now this seems fine.
-  BASE_SEND_URL = "http://bulk.smsinfy.com/web2sms.php?username=alex123&password=alex123&sender=KARUNA"
+  # BASE_SEND_URL = "http://bulk.smsinfy.com/web2sms.php?username=alex123&password=alex123&sender=KARUNA"
+  BASE_SEND_URL = "http://bulk.smsinfy.com/web2sms.php?username=karuna&password=karuna123&sender=KARUNA"
   def send_sms(mobile, message)
     url = BASE_SEND_URL + "&to=#{mobile}&message=#{CGI::escape(message)}"
     curl = Curl::Easy.http_get(url)
